ae313bfafd6189fe4d8e514775df34fb8601df21
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-04-10 19:44:38 +0900

    コメントを追加

--- Sources/ScienceTokyoPortalKit/ScienceTokyoPortalKit.swift
+++ Sources/ScienceTokyoPortalKit/ScienceTokyoPortalKit.swift
@@ -29,10 +29,11 @@ public struct ScienceTokyoPortal {
     
     /// TitechPortalにログイン
     /// - Parameter account: ログイン情報
+    /// - Returns: リソース一覧ページのHTML
     public func loginCommon(account: ScienceTokyoPortalAccount) async throws -> String {
         /// ユーザー名ページの取得
         let userNamePageHtml = try await fetchUserNamePage()
-        /// ユーザー名ページのバリデーション
+        /// ユーザー名ページのバリデーション(既にログインしている場合)
         if try validateResourceListPage(html: userNamePageHtml){
             throw ScienceTokyoPortalLoginError.alreadyLoggedin
         }
@@ -49,7 +50,7 @@ public struct ScienceTokyoPortal {
         
         /// ユーザー名Formの送信
         let userNamePageSubmitJson = try await submitUserName(htmlInputs: userNamePageInputs, htmlMetas: userNamePageMetas, username: account.username)
-        /// ユーザー名ページsubmisionのバリデーション
+        /// ユーザー名submisionのバリデーション
         guard try validateUserNamePageSubmitJson(json: userNamePageSubmitJson, account: account) else {
             throw ScienceTokyoPortalLoginError.invalidUserNamePage
         }
@@ -72,9 +73,15 @@ public struct ScienceTokyoPortal {
         return methodSelectionPageHtml
     }
     
+    /// 認証方法選択ページでEmailを選択した場合(前半)
+    /// - Parameters:
+    ///   - account: ログイン情報
+    ///   - methodSelectionPageHtml: 認証方法選択ページのHTML
+    /// - Returns: OTPページのInputタグとMetaタグ
     public func loginEmail(account: ScienceTokyoPortalAccount, methodSelectionPageHtml: String) async throws -> ([HTMLInput],[HTMLMeta]) {
         /// Metaタグの取得
         let methodSelectionPageMetas = try parseHTMLMeta(html: methodSelectionPageHtml)
+        /// EmailでOTP送信するリクエストの送信
         let emailSendingResult = try await submitEmailSending(htmlMetas: methodSelectionPageMetas)
         guard validateEmailSending(result: emailSendingResult) else {
             throw ScienceTokyoPortalLoginError.invalidEmailSending
@@ -86,51 +93,72 @@ public struct ScienceTokyoPortal {
         return (otpPageInputs, methodSelectionPageMetas)
     }
     
-    public func loginTOTP(account: ScienceTokyoPortalAccount, methodSelectionPageHtml: String) async throws{
+    /// 認証方法選択ページでTOTPを選択した場合
+    /// - Parameters:
+    ///   - account: ログイン情報
+    ///   - methodSelectionPageHtml: 認証方法選択ページのHTML
+    public func loginTOTP(account: ScienceTokyoPortalAccount, methodSelectionPageHtml: String) async throws {
         /// Metaタグの取得
         let methodSelectionPageMetas = try parseHTMLMeta(html: methodSelectionPageHtml)
         /// 一部のHTMLを取り出す
         let extractedOTPPageHtml = try extractHTML(html: methodSelectionPageHtml, cssSelector: "form#totp-form")
         /// Inputタグの取得
         let otpPageInputs = try parseHTMLInput(html: extractedOTPPageHtml)
+        /// TOTPを計算した上でリクエストを送信
         let otpPageSubmitScript = try await submitTOTP(htmlInputs: otpPageInputs, htmlMetas: methodSelectionPageMetas, account: account)
         /// OTPページsubmisionのバリデーション
         guard validateSubmitScript(script: otpPageSubmitScript) else {
             throw ScienceTokyoPortalLoginError.invalidTOTPPage
         }
-        
+        /// OTPページsubmisionのレスポンスからURLを取得
         let otpPageSubmitURL = try parseScriptToURL(script: otpPageSubmitScript)
+        /// otpPageSubmitURLのURLを元に、待機ページを取得
         let waitingPageHtml = try await fetchWaitingPage(url: otpPageSubmitURL)
+        /// 待機ページのバリデーション
         guard try validateWaitingPage(html: waitingPageHtml) else {
             throw ScienceTokyoPortalLoginError.invalidWaitingPage
         }
-        
+        /// Inputタグの取得
         let waitingPageHtmlInputs = try parseHTMLInput(html: waitingPageHtml)
-
+        /// リソース一覧ページを取得
         let resourceListPageHtml = try await fetchResourceListPage(htmlInputs: waitingPageHtmlInputs, referer: otpPageSubmitURL)
         guard try validateResourceListPage(html: resourceListPageHtml) else {
             throw ScienceTokyoPortalLoginError.invalidResourceListPage
         }
     }
     
+    /// EmailでOTPを送信した後の処理．OTPを入力してログインする
+    /// - Parameters:
+    ///   - htmlInputs: OTPページのInputタグ
+    ///   - htmlMetas: OTPページのMetaタグ
+    ///   - otp: OTP
     public func latterEmailLogin(htmlInputs: [HTMLInput], htmlMetas: [HTMLMeta], otp: String) async throws {
+        /// OTPを送信
         let otpPageSubmitScript = try await submitEmail(htmlInputs: htmlInputs, htmlMetas: htmlMetas, otp: otp)
         /// OTPページsubmisionのバリデーション
         guard validateSubmitScript(script: otpPageSubmitScript) else {
             throw ScienceTokyoPortalLoginError.invalidEmailPage
         }
+        /// OTPページsubmisionのレスポンスからURLを取得
         let otpPageSubmitURL = try parseScriptToURL(script: otpPageSubmitScript)
+        /// otpPageSubmitURLのURLを元に、待機ページを取得
         let waitingPageHtml = try await fetchWaitingPage(url: otpPageSubmitURL)
+        /// 待機ページのバリデーション
         guard try validateWaitingPage(html: waitingPageHtml) else {
             throw ScienceTokyoPortalLoginError.invalidWaitingPage
         }
+        /// Inputタグの取得
         let waitingPageHtmlInputs = try parseHTMLInput(html: waitingPageHtml)
+        /// リソース一覧ページを取得
         let resourceListPageHtml = try await fetchResourceListPage(htmlInputs: waitingPageHtmlInputs, referer: otpPageSubmitURL)
+        /// リソース一覧ページのバリデーション
         guard try validateResourceListPage(html: resourceListPageHtml) else {
             throw ScienceTokyoPortalLoginError.invalidResourceListPage
         }
     }
     
+    /// ログイン済みの状態でFIDO2を設定する
+    /// 現在のところ，fido2Relay2Jsonでエラーコードが返ってくる
     public func setFIDO2() async throws {
         let fido2PageHtml = try await fetchFIDO2Page()
         let fido2HtmlInput = try parseHTMLInput(html: fido2PageHtml)
@@ -143,7 +171,8 @@ public struct ScienceTokyoPortal {
     }
     
     /// UsernameとPasswordのみが正しいかチェック
-    /// - Parameter account: チェックするアカウント情報
+    /// - Parameter username: ユーザー名
+    /// - Parameter password: パスワード
     /// - Returns: 正しくログインできればtrue, エラーであればfalseを返す
     public func checkUsernamePassword(username: String, password: String) async throws -> Bool {
         let account = ScienceTokyoPortalAccount(username: username, password: password, totpSecret: nil)
@@ -166,7 +195,7 @@ public struct ScienceTokyoPortal {
         
         /// ユーザー名Formの送信
         let userNamePageSubmitJson = try await submitUserName(htmlInputs: userNamePageInputs, htmlMetas: userNamePageMetas, username: account.username)
-        /// ユーザー名ページsubmisionのバリデーション
+        /// ユーザー名submisionのバリデーション
         guard try validateUserNamePageSubmitJson(json: userNamePageSubmitJson, account: account) else {
             throw ScienceTokyoPortalLoginError.invalidUserNamePage
         }
@@ -181,14 +210,20 @@ public struct ScienceTokyoPortal {
     }
 
 
-    
+    /// ユーザー名ページを取得
+    /// - Returns: ユーザー名ページのHTML
     private func fetchUserNamePage() async throws -> String {
         let request = UserNamePageRequest()
         
         return try await httpClient.send(request)
     }
     
-    
+    /// ユーザー名formを送信
+    /// - Parameters:
+    ///   - htmlInputs: ユーザー名ページのInputタグ
+    ///   - htmlMetas: ユーザー名ページのMetaタグ
+    ///   - username: ユーザー名
+    /// - Returns: ユーザー名formの送信結果    
     private func submitUserName(htmlInputs: [HTMLInput], htmlMetas: [HTMLMeta], username: String) async throws -> String {
         let htmlMetas = htmlMetas.filter{ $0.name == "csrf-token" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.content )} // csrf-tokenを取り出す
         let htmlInputs = inject(htmlInputs, username: username, password: "")
@@ -197,6 +232,13 @@ public struct ScienceTokyoPortal {
         return try await httpClient.send(request)
     }
     
+    /// PasswordFormを送信
+    /// - Parameters:
+    ///   - htmlInputs: ユーザー名ページのInputタグ
+    ///   - htmlMetas: ユーザー名ページのMetaタグ
+    ///   - username: ユーザー名
+    ///   - password: パスワード
+    /// - Returns: PasswordFormの送信結果
     private func submitPassword(htmlInputs: [HTMLInput], htmlMetas: [HTMLMeta], username: String, password: String) async throws -> String {
         let htmlMetas = htmlMetas.filter{ $0.name == "csrf-token" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.content )} // csrf-tokenを取り出す
         let injectedHtmlInputs = inject(htmlInputs, username: username, password: password)
@@ -206,18 +248,29 @@ public struct ScienceTokyoPortal {
         return try await httpClient.send(request)
     }
     
+    /// 認証方法選択ページを取得
+    /// - Returns: 認証方法選択ページのHTML
     private func fetchAuthorizationMethodSelectionPage() async throws -> String {
         let request = AuthorizationMethodSelectionPageRequest()
         
         return try await httpClient.send(request)
     }
     
+    /// EmailでOTPを送信
+    /// - Parameter htmlMetas: 認証方法選択ページのMetaタグ
+    /// - Returns: JSON形式のOTP送信結果
     private func submitEmailSending(htmlMetas: [HTMLMeta]) async throws -> String {
         let htmlMetas = htmlMetas.filter{ $0.name == "csrf-token" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.content )} // csrf-tokenを取り出す
         let request = EmailSendingSubmitRequest(htmlMetas: htmlMetas)
         return try await httpClient.send(request)
     }
     
+    /// OTPを送信
+    /// - Parameters:
+    ///   - htmlInputs: 認証方法選択ページのInputタグ
+    ///   - htmlMetas: 認証方法選択ページのMetaタグ
+    ///   - otp: OTP
+    /// - Returns: OTPFormの送信結果
     private func submitEmail(htmlInputs: [HTMLInput], htmlMetas: [HTMLMeta], otp: String) async throws -> String {
         let htmlMetas = htmlMetas.filter{ $0.name == "csrf-token" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.content )} // csrf-tokenを取り出す
         let injectedHtmlInputs = inject(htmlInputs, username: otp, password: "")
@@ -225,6 +278,12 @@ public struct ScienceTokyoPortal {
         return try await httpClient.send(request)
     }
     
+    /// TOTPを送信
+    /// - Parameters:
+    ///   - htmlInputs: 認証方法選択ページのInputタグ
+    ///   - htmlMetas: 認証方法選択ページのMetaタグ
+    ///   - account: ログイン情報
+    /// - Returns: OTPFormの送信結果
     private func submitTOTP(htmlInputs: [HTMLInput], htmlMetas: [HTMLMeta], account: ScienceTokyoPortalAccount) async throws -> String {
         let htmlMetas = htmlMetas.filter{ $0.name == "csrf-token" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.content )} // csrf-tokenを取り出す
         guard let accountTotp = account.totpSecret else {
@@ -236,39 +295,63 @@ public struct ScienceTokyoPortal {
         return try await httpClient.send(request)
     }
     
+    /// 待機ページを取得
+    /// - Parameter url: 待機ページのURL
+    /// - Returns: 待機ページのHTML
     private func fetchWaitingPage(url: String) async throws -> String {
         let request = WaitingPageRequest(url: URL(string: url)!)
         return try await httpClient.send(request)
     }
     
+    /// リソース一覧ページを取得
+    /// - Parameters:
+    ///   - htmlInputs: 待機ページのInputタグ
+    ///   - referer: 待機ページのURL
+    /// - Returns: リソース一覧ページのHTML
     private func fetchResourceListPage(htmlInputs: [HTMLInput], referer: String) async throws -> String {
         let request = ResourceListPageRequest(htmlInputs: htmlInputs, referer: referer)
         return try await httpClient.send(request)
     }
     
+    /// FIDO2設定ページを取得
+    /// - Returns: FIDO2設定ページのHTML
     private func fetchFIDO2Page() async throws -> String {
         let request = FIDO2PageRequest()
         return try await httpClient.send(request)
     }
     
+    /// FIDO2設定を送信
+    /// - Parameter htmlInput: FIDO2設定ページのInputタグ
+    /// - Returns: FIDO2設定の送信結果
     private func submitFIDO2Settings(htmlInput: [HTMLInput]) async throws -> String {
         let htmlMetas = htmlInput.filter{ $0.name == "_csrf" }.map{ HTMLMeta(name: "x-csrf-token", content: $0.value )} // csrf-tokenを取り出す
         let request = FIDO2SettingsRequest(htmlMetas: htmlMetas)
         return try await httpClient.send(request)
     }
     
+    /// FIDO2 Relay(1回目)を送信
+    /// - Parameter htmlInput: FIDO2設定ページのInputタグ
+    /// - Returns: FIDO2 Relay1の送信結果
     private func submitFIDO2Relay1(htmlInput: [HTMLInput]) async throws -> String {
         let htmlMetas = htmlInput.filter{ $0.name == "_csrf" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.value )} // csrf-tokenを取り出す
         let request = FIDO2Relay1Request(htmlMetas: htmlMetas)
         return try await httpClient.send(request)
     }
     
+    /// FIDO2 Relay(2回目)を送信
+    /// - Parameters:
+    ///   - htmlInput: FIDO2設定ページのInputタグ
+    ///   - jsonBody: FIDO2 Relay1のレスポンスから計算したJSON
+    /// - Returns: FIDO2 Relay2の送信結果
     private func submitFIDO2Relay2(htmlInput: [HTMLInput], jsonBody: [String: Any]) async throws -> String {
         let htmlMetas = htmlInput.filter{ $0.name == "_csrf" }.map{ HTMLMeta(name: "X-CSRF-Token", content: $0.value )} // csrf-tokenを取り出す
         let request = FIDO2Relay2Request(htmlMetas: htmlMetas, jsonBody: jsonBody)
         return try await httpClient.send(request)
     }
     
+    /// ユーザー名ページのバリデーション
+    /// - Parameter html: ユーザー名ページのHTML
+    /// - Returns: ユーザー名ページが正しい場合はtrue, エラーであればfalseを返す
     private func validateUserNamePage(html: String) throws -> Bool {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -277,6 +360,11 @@ public struct ScienceTokyoPortal {
         return bodyHtml.contains("Please set your e-mail address for password reissue to an e-mail other than m.isct.ac.jp.") || bodyHtml.contains("パスワード再発行用メールアドレスをm.isct.ac.jp以外のメールアドレスに忘れず必ず設定してください。")
     }
     
+    /// ユーザー名Formのsubmisionのバリデーション
+    /// - Parameters:
+    ///   - json: ユーザー名FormのsubmisionのJSON
+    ///   - account: ログイン情報
+    /// - Returns: ユーザー名Formのsubmisionが正しい場合はtrue, エラーであればfalseを返す
     private func validateUserNamePageSubmitJson(json: String, account: ScienceTokyoPortalAccount) throws -> Bool {
         let jsonObject = try JSONSerialization.jsonObject(with: Data(json.utf8), options: [])
         guard let jsonDict = jsonObject as? [String: Any] else {
@@ -290,11 +378,18 @@ public struct ScienceTokyoPortal {
         }
         return password && username == account.username
     }
-        
+    
+    /// PasswordFormのsubmisionのバリデーション
+    /// - Parameter script: PasswordFormのsubmisionのscript
+    /// - Returns: PasswordFormのsubmisionが正しい場合はtrue, エラーであればfalseを返す
     private func validateSubmitScript(script: String) -> Bool {
         return script.contains("window.location=")
     }
     
+    /// 認証方法選択ページのバリデーション
+    /// - Parameter html: 認証方法選択ページのHTML
+    /// - Returns: 認証方法選択ページが正しい場合はtrue, エラーであればfalseを返す
+    /// - Note: 認証方法選択ページは、認証方法を選択するためのページであることを確認する
     private func validateMethodSelectionPage(html: String) throws -> Bool {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -303,6 +398,9 @@ public struct ScienceTokyoPortal {
         return bodyHtml.contains("Please select an authentication method.") || bodyHtml.contains("認証方法を選択してください。")
     }
 
+    /// 待機ページのバリデーション
+    /// - Parameter html: 待機ページのHTML
+    /// - Returns: 待機ページが正しい場合はtrue, エラーであればfalseを返す
     private func validateWaitingPage(html: String) throws -> Bool {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -311,6 +409,9 @@ public struct ScienceTokyoPortal {
         return bodyHtml.contains("Please wait for a moment") || bodyHtml.contains("しばらくお待ちください。")
     }
     
+    /// リソース一覧ページのバリデーション
+    /// - Parameter html: リソース一覧ページのHTML
+    /// - Returns: リソース一覧ページが正しい場合はtrue, エラーであればfalseを返す
     private func validateResourceListPage(html: String) throws -> Bool {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -319,15 +420,26 @@ public struct ScienceTokyoPortal {
         return bodyHtml.contains("Account") || bodyHtml.contains("アカウント")
     }
     
+    /// Email送信のバリデーション
+    /// - Parameter result: Email送信の結果
+    /// - Returns: Email送信が正しい場合はtrue, エラーであればfalseを返す
     private func validateEmailSending(result: String) -> Bool {
         return result.contains("succeeded")
     }
     
+    /// HTMLから特定の部分を抽出する
+    /// - Parameters:
+    ///   - html: HTML
+    ///   - cssSelector: CSSセレクタ
+    /// - Returns: 抽出したHTML
     func extractHTML(html: String, cssSelector: String) throws -> String {
         let doc = try HTML(html: html, encoding: .utf8)
         return doc.body?.css(cssSelector).first?.innerHTML ?? ""
     }
     
+    /// HTMLからInputタグを取得する
+    /// - Parameter html: HTML
+    /// - Returns: Inputタグの配列
     func parseHTMLInput(html: String) throws -> [HTMLInput] {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -340,6 +452,9 @@ public struct ScienceTokyoPortal {
         }
     }
     
+    /// HTMLからSelectタグを取得する
+    /// - Parameter html: HTML
+    /// - Returns: Selectタグの配列
     func parseHTMLSelect(html: String) throws -> [HTMLSelect] {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -351,6 +466,9 @@ public struct ScienceTokyoPortal {
         }
     }
     
+    /// HTMLからMetaタグを取得する
+    /// - Parameter html: HTML
+    /// - Returns: Metaタグの配列
     func parseHTMLMeta(html: String) throws -> [HTMLMeta] {
         let doc = try HTML(html: html, encoding: .utf8)
         
@@ -362,11 +480,20 @@ public struct ScienceTokyoPortal {
         }
     }
     
+    /// HTMLからScriptタグを取得する
+    /// - Parameter html: HTML
+    /// - Returns: Scriptタグの配列
     func parseScriptToURL(script: String) throws -> String {
         let components = script.components(separatedBy: "\"")
         return components[1]
     }
     
+    /// HTMLInputの配列に値を注入する
+    /// - Parameters:
+    ///   - inputs: HTMLInputの配列
+    ///   - username: ユーザー名
+    ///   - password: パスワード
+    /// - Returns: 値を注入したHTMLInputの配列
     func inject(_ inputs: [HTMLInput], username: String, password: String) -> [HTMLInput] {
         var inputs = inputs
         if let firstTextInput = inputs.first(where: { $0.type == .text })
