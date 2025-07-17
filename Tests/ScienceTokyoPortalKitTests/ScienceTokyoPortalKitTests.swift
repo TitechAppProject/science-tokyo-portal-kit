19cb7cb7438ac1edd0125b1dd63320aa7192411c
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-07-17 23:34:16 +0900

    testコードの初期化

--- Tests/ScienceTokyoPortalKitTests/ScienceTokyoPortalKitTests.swift
+++ Tests/ScienceTokyoPortalKitTests/ScienceTokyoPortalKitTests.swift
@@ -3,12 +3,15 @@ import Testing
 
 @testable import ScienceTokyoPortalKit
 
-//
-//@Test func testTOTPLogin() async throws {
-//    let account = ScienceTokyoPortalAccount(username: "username", password: "password", totpSecret: "xxxxxxxx")
-//    let portal = ScienceTokyoPortal(urlSession: .shared)
-//    let loginPageHtml = try await portal.loginCommon(account: account)
-//    try await portal.loginTOTP(account: account, methodSelectionPageHtml: loginPageHtml)
-//    try await portal.getLMSDashboard()
-//    print("wstoken=", try await portal.getLMSToken())
-//}
+struct ScienceTokyoPortalKitTests {
+/*
+    @Test func testTOTPLogin() async throws {
+        let account = ScienceTokyoPortalAccount(username: "username", password: "password", totpSecret: "xxxxxxxx")
+        let portal = ScienceTokyoPortal(urlSession: .shared)
+        let loginPageHtml = try await portal.loginCommon(account: account)
+        try await portal.loginTOTP(account: account, methodSelectionPageHtml: loginPageHtml)
+        try await portal.getLMSDashboard()
+        print("wstoken=", try await portal.getLMSToken())
+    }
+*/
+}
