d9c257c1456f5555c9e264b55e881ffa13cac342
中島正矩 <nakaji-m@nakajimamasanorinoMacBook-Pro.local>
2025-07-18 12:26:49 +0900

    inputのparse処理のテストを追加

--- Tests/ScienceTokyoPortalKitTests/ScienceTokyoPortalKitTests.swift
+++ Tests/ScienceTokyoPortalKitTests/ScienceTokyoPortalKitTests.swift
@@ -36,6 +36,25 @@ final class ScienceTokyoPortalKitTests: XCTestCase {
         XCTAssertTrue(try! portal.validateUserNamePageSubmitJson(json: json, account: account))
     }
 
+    func testParsePasswordPageInputs() throws {
+        let portal = ScienceTokyoPortal(urlSession: .shared)
+
+        let html = try! String(contentsOf: Bundle.module.url(forResource: "UserNamePage", withExtension: "html")!)
+
+        let extractedHtml = try! portal.extractHTML(html: html, cssSelector: "form#login")
+        let inputs = try! portal.parseHTMLInput(html: extractedHtml)
+
+        XCTAssertEqual(
+            inputs,
+            [
+                HTMLInput(name: "utf8", type: .hidden, value: "✓"),
+                HTMLInput(name: "authenticity_token", type: .hidden, value: "-ZZOqjVkdg1heh6dmdxsoUfrx6PIB04RslZIjk3EW79lq11bk7lhErp4seE34Bzy23959IC2DUVeUUA2FSMWhA"),
+                HTMLInput(name: "identifier", type: .text, value: ""),
+                HTMLInput(name: "password", type: .password, value: ""),
+            ]
+        )
+    }
+
     func testPasswordPageSubmitScriptValidation() throws {
         let portal = ScienceTokyoPortal(urlSession: .shared)
 
@@ -50,6 +69,24 @@ final class ScienceTokyoPortalKitTests: XCTestCase {
         let html = try! String(contentsOf: Bundle.module.url(forResource: "MethodSelectionPage", withExtension: "html")!)
 
         XCTAssertTrue(try! portal.validateMethodSelectionPage(html: html))
+    }
+
+    func testParseOTPPageInputs() throws {
+        let portal = ScienceTokyoPortal(urlSession: .shared)
+
+        let html = try! String(contentsOf: Bundle.module.url(forResource: "MethodSelectionPage", withExtension: "html")!)
+
+        let extractedHtml = try! portal.extractHTML(html: html, cssSelector: "form#totp-form")
+        let inputs = try! portal.parseHTMLInput(html: extractedHtml)
+
+        XCTAssertEqual(
+            inputs,
+            [
+                HTMLInput(name: "utf8", type: .hidden, value: "✓"),
+                HTMLInput(name: "authenticity_token", type: .hidden, value: "-sXIN3BuAlYSqBeHqvXL46jh_jA4z06hgfed9ymvtH6PawKonTII3Z_TpmORb2f7vgDx37Rk8jtBRTN1gA_MIA"),
+                HTMLInput(name: "totp", type: .text, value: ""),
+            ]
+        )
     }
 
     func testOtpPageSubmitScriptValidation() throws {
